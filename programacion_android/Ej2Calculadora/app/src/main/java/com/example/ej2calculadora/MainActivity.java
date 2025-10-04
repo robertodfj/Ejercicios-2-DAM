6e5f3caef7fa623889b384b6826a2b65edfd71c9
Roberto de Frutos Jiménez <robertodfj93@gmail.com>
2025-10-05 01:18:41 +0200

    Proyecto calculadora finalizado a falta del historial de operaciones

--- programacion_android/Ej2Calculadora/app/src/main/java/com/example/ej2calculadora/MainActivity.java
+++ programacion_android/Ej2Calculadora/app/src/main/java/com/example/ej2calculadora/MainActivity.java
@@ -1,25 +1,44 @@
 package com.example.ej2calculadora;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9,
+            btnPunto, btnMas, btnMenos, btnDiv, btnMulti, btnAc, btnBorrar, btnIgual,
+
+            btnModo;
+
+    TextView textView;
+    ConstraintLayout mainLayout;
+
+    Double numero1 = (double) 0;
+    Double numero2 = (double) 0;
+    boolean nuevoNumero = true;
+    char operador;
+
+    String operacion = "0";
+
+    Boolean modo = true;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+        // Numeros
         btn1 = (Button) findViewById(R.id.btn1);
         btn2 = (Button) findViewById(R.id.btn2);
         btn3 = (Button) findViewById(R.id.btn3);
@@ -31,22 +50,119 @@ public class MainActivity extends AppCompatActivity {
         btn9 = (Button) findViewById(R.id.btn9);
         btn0 = (Button) findViewById(R.id.btn0);
 
-        // Tenemos que guardar los numeros como num1 y concatenarlos y pasarlos a double o int hasta pulsar el signo de op
-        // Hacemos lo mismo con num2 hasta pulsar =, realizamos la operacion
+        // Char
+        btnPunto = (Button) findViewById(R.id.btnPunto);
+        btnMas = (Button) findViewById(R.id.btnMas);
+        btnMenos = (Button) findViewById(R.id.btnMenos);
+        btnDiv = (Button) findViewById(R.id.btnDivision);
+        btnMulti = (Button) findViewById(R.id.btnMultiplicar);
+        btnIgual = (Button) findViewById(R.id.btnIgual);
 
-        // Añadir numero1 char numero 2
-        // Añadir boolean si es nuevo numero o no
-        // Añadir onclick listeners
+        // Acciones
+        btnAc = (Button) findViewById(R.id.btnAC);
+        btnBorrar = (Button) findViewById(R.id.btnBorrar);
+        btnModo = (Button) findViewById(R.id.btnModo);
+        textView = (TextView) findViewById(R.id.textView);
+        mainLayout = findViewById(R.id.main);
 
-        List<String> operacion = new ArrayList<>();
-        // Se usara para imprimir la operacion completa
 
+        // Listener de numeros y operadores:
+        btn0.setOnClickListener(v -> agregarNumero("0"));
+        btn1.setOnClickListener(v -> agregarNumero("1"));
+        btn2.setOnClickListener(v -> agregarNumero("2"));
+        btn3.setOnClickListener(v -> agregarNumero("3"));
+        btn4.setOnClickListener(v -> agregarNumero("4"));
+        btn5.setOnClickListener(v -> agregarNumero("5"));
+        btn6.setOnClickListener(v -> agregarNumero("6"));
+        btn7.setOnClickListener(v -> agregarNumero("7"));
+        btn8.setOnClickListener(v -> agregarNumero("8"));
+        btn9.setOnClickListener(v -> agregarNumero("9"));
+        btnMas.setOnClickListener(v -> agregarOperador('+'));
+        btnMenos.setOnClickListener(v -> agregarOperador('-'));
+        btnMulti.setOnClickListener(v -> agregarOperador('*'));
+        btnDiv.setOnClickListener(v -> agregarOperador('/'));
+        btnPunto.setOnClickListener(v -> agregarPunto());
+        btnModo.setOnClickListener(v -> cambioModo());
+
+        btnIgual.setOnClickListener(v -> calcular());
+
+        btnBorrar.setOnClickListener(v -> limpiar());
+        btnAc.setOnClickListener(v -> limpiar());
 
 
     }
 
-    public void agregarNumero(String numero){
+    public void agregarNumero(String numeroPulsado){
+        if (nuevoNumero) {
+            nuevoNumero = false;
+            operacion = numeroPulsado.toString();
+            textView.setText(numeroPulsado);
+        } else {
+            operacion = operacion + numeroPulsado;
+            textView.setText(operacion);
+        }
+    }
 
+    public void agregarPunto(){
+        if (nuevoNumero){
+            textView.setText("0.");
+            operacion = "0.";
+        } else {
+            String operacionExistente = textView.getText().toString();
+            if (!operacionExistente.contains(".")) {
+                textView.setText(operacionExistente + ".");
+                operacion = operacion + ".";
+            }
+        }
+    }
+
+    public void agregarOperador(Character operadorPulsado){
+        numero1 = Double.parseDouble(textView.getText().toString());
+        operacion = operacion + operadorPulsado;
+        textView.setText(operacion);
+        operador = operadorPulsado;
+        nuevoNumero = true;
+    }
+
+    public void calcular(){
+        numero2 = Double.parseDouble(textView.getText().toString());
+        double resultado = 0;
+        switch (operador){
+            case '+': resultado = numero1 + numero2; break;
+            case '-': resultado = numero1 - numero2; break;
+            case '*': resultado = numero1 * numero2; break;
+            case '/': if (numero2 != 0){
+                    resultado = numero1 / numero2;
+                } else {
+                    textView.setText("Sin definir");
+                    nuevoNumero = true;
+                    return;
+                } break;
+        }
+
+        textView.setText(String.valueOf(resultado));
+        nuevoNumero = true;
+    }
+
+    public void limpiar(){
+        operacion = "";
+        nuevoNumero = true;
+        textView.setText("0");
+    }
+
+    public void cambioModo(){
+        if (modo){
+            btnModo.setText("🌜");
+            modo = false;
+            mainLayout.setBackgroundColor(getResources().getColor(android.R.color.white));
+            textView.setTextColor(getResources().getColor(android.R.color.black));
+
+        } else {
+            modo = true;
+            btnModo.setText("☀️");
+            mainLayout.setBackgroundColor(getResources().getColor(android.R.color.black));
+            textView.setTextColor(getResources().getColor(android.R.color.white));
+        }
     }
 
 }
