e577f11a1e3d7a529e63664bf3bd99545fec260f
Roberto de Frutos Jiménez <robertodfj93@gmail.com>
2025-10-18 00:01:00 +0200

    Creamos el ejercicio entregable2, aplicamos los cambios creando las actividades necesarias y añadiendo diseño a la pantalla prinipal y la de juego

---
+++ DesarrolloInterfaces/Entregables/app/src/main/java/com/example/ej2adivinanza/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.ej2adivinanza;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btnJuego = findViewById(R.id.btnJuego);
+        Button btnAjustes = findViewById(R.id.btnAjustes);
+        Button btnHistorial = findViewById(R.id.btnHistorio);
+
+
+        btnJuego.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i1 = new Intent(MainActivity.this, Juego.class);
+                startActivity(i1);
+            }
+        });
+
+        btnHistorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i2 = new Intent(MainActivity.this, Ajustes.class);
+                startActivity(i2);
+            }
+        });
+
+        btnAjustes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i3 = new Intent(MainActivity.this, Historial.class);
+                startActivity(i3);
+            }
+        });
+    }
+}
