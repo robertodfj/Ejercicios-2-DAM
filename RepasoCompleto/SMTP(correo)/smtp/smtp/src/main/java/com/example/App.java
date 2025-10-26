2d3feaa16ba8ffd1a297e053f15bbbd6e0e1d15d
Roberto de Frutos Jiménez <robertodfj93@gmail.com>
2025-10-26 18:40:44 +0100

    Realizamos el ej1 de SMTP

--- RepasoCompleto/SMTP(correo)/smtp/smtp/src/main/java/com/example/App.java
+++ RepasoCompleto/SMTP(correo)/smtp/smtp/src/main/java/com/example/App.java
@@ -1,8 +1,11 @@
 package com.example;
 
 import java.io.PrintWriter;
+import java.io.Writer;
 
+import org.apache.commons.net.PrintCommandListener;
 import org.apache.commons.net.smtp.AuthenticatingSMTPClient;
+import org.apache.commons.net.smtp.SimpleSMTPHeader;
 public class App 
 {
     public static void main( String[] args )
@@ -10,7 +13,7 @@ public class App
         // Elementos necesarios para enviar un correo
         String servidor = "smtp.gmail.com";
         String emisor = "robertodfj93@gmail.com";
-        String password = ""; // Usamos APP pasword
+        String password = ""; // Usamos APP pasword (Borrada para realizar commit)
         String receptor = "paulajuagon2008@gmail.com";
 
         try {
@@ -25,6 +28,35 @@ public class App
 			
 			// Autenticacion
 			smtp.auth(AuthenticatingSMTPClient.AUTH_METHOD.PLAIN, emisor, password);
+
+            // Vinculamos emisor y receptor
+            smtp.setSender(emisor);
+            smtp.addRecipient(receptor);
+            
+
+            // Construimos mensaje
+            String asunto = "Correo desde Java";
+            String mensaje = "Hola, este correo ha sido enviado desde una aplicacion Java usando Apache";
+            SimpleSMTPHeader header = new SimpleSMTPHeader(emisor, receptor, asunto);
+
+            Writer wr = smtp.sendMessageData();
+            if(wr != null) {
+                wr.write(header.toString());
+                wr.write(mensaje);
+                wr.close();
+            }
+
+            // Comprobamos la conexion:
+            if(!smtp.completePendingCommand()) {
+                System.out.println("Mensaje no enviado, error :(");
+            } else {
+                System.out.println("Email enviado! :)");
+            }
+
+            // Cerrar conexion
+            smtp.logout();
+            smtp.disconnect();
+                
 			
         } catch (Exception e) {
             // TODO: handle exception
