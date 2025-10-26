8b7faacf411b5876b5e246b17915ed74e99346a9
Roberto de Frutos Jiménez <robertodfj93@gmail.com>
2025-10-26 18:50:57 +0100

    Realizamos el ej2 de SMTP

---
+++ RepasoCompleto/SMTP(correo)/SMTP2.java
@@ -0,0 +1,58 @@
+package com.example;
+
+import java.io.PrintWriter;
+import java.io.Writer;
+
+import org.apache.commons.net.PrintCommandListener;
+import org.apache.commons.net.smtp.AuthenticatingSMTPClient;
+import org.apache.commons.net.smtp.SimpleSMTPHeader;
+
+public class SMTP2 {
+    public static void main(String[] args) {
+        String servidor = "smtp.gmail.com";
+        String emisor = "robertodfj93@gmail.com";
+        String password = "uaiqjghmeudgeyev"; // Usamos APP pasword
+        String receptor = "robertodfj93@gmail.com";
+
+        try {
+            AuthenticatingSMTPClient smtp = new AuthenticatingSMTPClient("TLS", true);
+            smtp.setDefaultTimeout(15000);
+            smtp.addProtocolCommandListener(new PrintCommandListener(new PrintWriter(System.out, true)));
+
+            // Conectamos con el server
+            smtp.connect(servidor, 465);
+            smtp.ehlo("localhost"); // Linea importante (Saludo inicial)
+
+            // Vinculamos emisor y receptor
+            smtp.setSender(emisor);
+            smtp.addRecipient(receptor);
+
+            // Autenticacion
+            smtp.auth(AuthenticatingSMTPClient.AUTH_METHOD.PLAIN, emisor, password);
+
+            // Construir mensaje
+            String asunto = "Correo desde Java - SMTP2";
+            String mensaje = "Hola, este correo ha sido enviado desde una aplicacion Java usando Apache - SMTP2";
+            SimpleSMTPHeader header = new SimpleSMTPHeader(emisor, receptor, asunto);
+
+            Writer wr = smtp.sendMessageData();
+
+            if (wr != null) {
+                wr.write(header.toString());
+                wr.write(mensaje);
+                wr.close();
+            }
+
+            if(smtp.completePendingCommand()){
+                System.out.println("Email enviado! :)");
+            } else {
+                System.out.println("Mensaje no enviado, error :(");
+            }
+
+            smtp.logout();
+            smtp.disconnect();
+        } catch (Exception e) {
+            System.out.println("Error al enviar el correo: " + e.getMessage());
+        }
+    }
+}
